81b0c7f7b61ac332338ff093fc2ed75328ec9212
anwarkvzk <anvarvzk521@gmail.com>
2023-09-04 23:14:17 +0530

    Login Form Created

--- src/pages/auth/Login.jsx
+++ src/pages/auth/Login.jsx
@@ -1,9 +1,35 @@
-import React from 'react'
+import React from "react";
+import InputType from "../../components/shared/form/InputType";
 
 function Login() {
   return (
-    <div>Login</div>
-  )
+    <>
+      <div className="row">
+        <div className="col-md-8 form-banner">
+          <img src="./assets/images/banner1.jpg" alt="" />
+        </div>
+        <div className="col-md-4 form-container">
+          <form>
+            <InputType labelText={"Email"} labelFor={"forEmail"} inputType={'email'} name={'email'}/>
+            <div className="mb-3">
+              <label htmlFor="exampleInputPassword1" className="form-label">
+                Password
+              </label>
+              <input
+                type="password"
+                className="form-control"
+                id="exampleInputPassword1"
+              />
+            </div>
+
+            <button type="submit" className="btn btn-primary">
+              Submit
+            </button>
+          </form>
+        </div>
+      </div>
+    </>
+  );
 }
 
-export default Login
+export default Login;
