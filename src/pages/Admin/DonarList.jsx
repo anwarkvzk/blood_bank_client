f273e8c5f20f738fba0a6a51f8d6f06563580c76
anwarkvzk <anvarvzk521@gmail.com>
2023-09-23 10:42:06 +0530

    Admin Panel Completed

--- src/pages/Admin/DonarList.jsx
+++ src/pages/Admin/DonarList.jsx
@@ -1,11 +1,76 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Layout from "../../components/shared/Layout/Layout";
+import moment from "moment";
+import API from "../../services/API";
 
 const DonarList = () => {
+  const [data, setData] = useState([]);
+  //find donar Records
+  const getDonars = async () => {
+    try {
+      const { data } = await API.get("/admin/donar-list");
+      // console.log(data);
+      if (data?.success) {
+        setData(data?.donarData);
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  useEffect(() => {
+    getDonars();
+  }, []);
+
+  //Delete Function
+  const handleDeletem = async (id) => {
+    try {
+      let answer = window.prompt(
+        "Are You Sure Want To Delete This Donar",
+        "Sure"
+      );
+      if (!answer) return;
+      const { data } = await API.delete(`/admin/delete-donar/${id}`);
+      alert(data?.message);
+      window.location.reload();
+    } catch (error) {
+      console.log(error);
+    }
+  };
   return (
-    <Layout>
-      <h1>DonarList</h1>
-    </Layout>
+    <>
+      <Layout>
+        <table className="table">
+          <thead>
+            <tr>
+              <th scope="col">Name</th>
+              <th scope="col">Email </th>
+              <th scope="col">Phone</th>
+              <th scope="col">Date</th>
+              <th scope="col">Action</th>
+            </tr>
+          </thead>
+          <tbody>
+            {data?.map((record) => (
+              <tr key={record._id}>
+                <td>{record.name || record.organisationName + " (ORG) "}</td>
+                <td>{record.email}</td>
+                <td>{record.phone}</td>
+                <td>{moment(record.createdAt).format("DD/MM/YYYY hh:mm A")}</td>
+                <td>
+                  <button
+                    className="btn btn-danger"
+                    onClick={() => handleDeletem(record._id)}
+                  >
+                    Delete
+                  </button>
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </Layout>
+    </>
   );
 };
 
--- src/pages/Admin/HospitalList.jsx
+++ src/pages/Admin/HospitalList.jsx
@@ -1,12 +1,79 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Layout from "../../components/shared/Layout/Layout";
+import moment from "moment";
+import API from "../../services/API";
 
 const HospitalList = () => {
-  return (
-    <Layout>
-      <h1>HospitalList</h1>
-    </Layout>
-  );
+   const [data, setData] = useState([]);
+   //find donar Records
+   const getDonars = async () => {
+     try {
+       const { data } = await API.get("/admin/hospital-list");
+       console.log(data);
+       if (data?.success) {
+         setData(data?.hospitalData);
+       }
+     } catch (error) {
+       console.log(error);
+     }
+   };
+
+   useEffect(() => {
+     getDonars();
+   }, []);
+
+   //Delete Function
+   const handleDelete = async (id) => {
+     try {
+       let answer = window.prompt(
+         "Are You Sure Want To Delete This Hospital",
+         "Sure"
+       );
+       if (!answer) return;
+       const { data } = await API.delete(`/admin/delete-donar/${id}`);
+       alert(data?.message);
+       window.location.reload();
+     } catch (error) {
+       console.log(error);
+     }
+   };
+   return (
+     <>
+       <Layout>
+         <table className="table">
+           <thead>
+             <tr>
+               <th scope="col">Name</th>
+               <th scope="col">Email </th>
+               <th scope="col">Phone</th>
+               <th scope="col">Date</th>
+               <th scope="col">Action</th>
+             </tr>
+           </thead>
+           <tbody>
+             {data?.map((record) => (
+               <tr key={record._id}>
+                 <td>{record.hospitalName}</td>
+                 <td>{record.email}</td>
+                 <td>{record.phone}</td>
+                 <td>
+                   {moment(record.createdAt).format("DD/MM/YYYY hh:mm A")}
+                 </td>
+                 <td>
+                   <button
+                     className="btn btn-danger"
+                     onClick={() => handleDelete(record._id)}
+                   >
+                     Delete
+                   </button>
+                 </td>
+               </tr>
+             ))}
+           </tbody>
+         </table>
+       </Layout>
+     </>
+   );
 };
 
 export default HospitalList;
